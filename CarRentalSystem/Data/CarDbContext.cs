442f3ebf2f693cbee0269a6aa82f0a2975ccf2b8
Gandjurov <gandjurov@abv.bg>
2018-12-30 15:20:58 +0200

    add skeleton

---
+++ CarRentalSystem/Data/CarDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace CarRentalSystem.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public class CarsDbContext : IdentityDbContext<User>
+    {
+        public CarsDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static CarsDbContext Create()
+        {
+            return new CarsDbContext();
+        }
+    }
+}
