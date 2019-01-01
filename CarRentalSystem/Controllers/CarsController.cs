8b9c02a55b58f21b88e0dcff4821df947754ac04
Gandjurov <gandjurov@abv.bg>
2019-01-01 18:58:16 +0200

    add Details

--- CarRentalSystem/Controllers/CarsController.cs
+++ CarRentalSystem/Controllers/CarsController.cs
@@ -3,6 +3,7 @@
     using CarRentalSystem.Data;
     using CarRentalSystem.Models.Cars;
     using Microsoft.AspNet.Identity;
+    using System.Linq;
     using System.Web.Mvc;
 
     public class CarsController : Controller
@@ -48,5 +49,33 @@
             return View(carModel);
         }
 
+        public ActionResult Details(int id)
+        {
+            var db = new CarsDbContext();
+
+            var car = db.Cars
+                .Where(c => c.Id == id)
+                .Select(c => new CarDetailsModel
+                {
+                    Color = c.Color,
+                    Engine = c.Engine,
+                    EngineType = c.EngineType,
+                    ImageUrl = c.ImageUrl,
+                    Make = c.Make,
+                    Model = c.Model,
+                    Power = c.Power,
+                    PricePerDay = c.PricePerDay,
+                    Year = c.Year,
+                    ContactInformation = c.Owner.Email
+                })
+                .FirstOrDefault();
+
+            if (car == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(car);
+        }
     }
 }
---
+++ CarRentalSystem/Models/Cars/CarDetailsModel.cs
@@ -0,0 +1,27 @@
+﻿namespace CarRentalSystem.Models.Cars
+{
+    using CarRentalSystem.Data;
+
+    public class CarDetailsModel
+    {
+        public string Make { get; set; }
+        
+        public string Model { get; set; }
+        
+        public int Year { get; set; }
+
+        public string Color { get; set; }
+
+        public double Engine { get; set; }
+
+        public EngineType EngineType { get; set; }
+
+        public int? Power { get; set; }
+        
+        public decimal PricePerDay { get; set; }
+        
+        public string ImageUrl { get; set; }
+
+        public string ContactInformation { get; set; }
+    }
+}
